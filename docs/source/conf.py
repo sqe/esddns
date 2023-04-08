f3caaabdbfa0289e416325891cb319a28a31120c
sqe <azizbek@gmail.com>
2023-04-07 19:38:06 -0700

    remove more extra cruft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,26 +8,13 @@
 import os
 import sys
 
-# print(os.environ["API_KEY"],"")
-
-# sys.path.insert(0, os.path.abspath(os.path.join('../esddns/esddns_service')))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-# sys.path.insert(0, os.path.abspath('/home/sqe/esdns/esddns/dns.ini'))
-sys.path.append(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 sys.path.insert(0, os.path.abspath("../.."))
-# sys.path.insert(0, os.path.abspath(os.path.join('..')))
-
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path.insert(0, basedir)
-print(sys.path)
 
 project = 'ESDDNS'
 copyright = '2023, Aziz Kurbanov'
 author = 'Aziz Kurbanov'
 release = '0.0.2'
 
-# import esddns.esddns_service 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
